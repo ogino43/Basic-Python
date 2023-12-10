f98cf50bb408bdd493893464269b2f102349167f
ogino43 <c.ito.23.11.1236@4nonome.com>
2023-12-10 22:39:55 +0900

    answer

--- euclid.py
+++ euclid.py
@@ -1,4 +1,24 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
-
+a = int(input("a の値を入力: "))
+b = int(input("b の値を入力: "))
 # TODO
+
+surplus = 0
+large = 0
+small = 0
+
+if a >= b:
+    large = a
+    small = b
+else:
+    large = b
+    small = a
+
+
+while True:
+    surplus = large % small
+    if surplus == 0:
+        print("最大公約数は%d" % small)
+        break
+    else:
+        large = small
+        small = surplus
